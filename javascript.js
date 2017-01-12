9a373398eadb5a5e8538ab60436cda1290f4b7b8
leahjs <leahj.suter@gmail.com>
2017-01-12 13:49:35 -0500

    added overflow styles to body

--- javascript.js
+++ javascript.js
@@ -1,36 +1,5 @@
-        //
-        // $(document).ready(function(){
-        //     $(".col-1").mouseenter(function(){
-        //         $(".inner-col-1b").fadeOut();
-        //         $(".inner-col-2b, .inner-col-3b, .inner-col-4b").fadeIn();
-        //     });
-        //     $(".col-2").mouseenter(function(){
-        //         $(".inner-col-2b").fadeOut();
-        //         $(".inner-col-1b,.inner-col-3b,.inner-col-4b").fadeIn();
-        //     });
-        //     $(".col-3").mouseenter(function(){
-        //         $(".inner-col-3b").fadeOut();
-        //         $(".inner-col-2b, .inner-col-1b, .inner-col-4b").fadeIn();
-        //     });
-        //     $(".col-4").mouseenter(function(){
-        //         $(".inner-col-4b").fadeOut();
-        //         $(".inner-col-2b, .inner-col-3b, .inner-col-1b").fadeIn();
-        //     });
-        //     /////////////////////
-        //     $("a.link-label-about").click(function(){
-        //         $("p.link-info, a.link-label").fadeOut();
-        //         $(".col-1").animate({ width: '100vw',minWidth: '800px'})
-        //             $(".col-2,.col-3,.col-4").fadeOut();
-        //             $(".link-info-wrap").fadeIn();
-        //     });
-        //     $("h1.about-exit").click(function(){
-        //         $("a.link-label-about").fadeIn();
-        //             $("p.link-info, a.link-label").fadeIn();
-        //             $(".col-1").animate({width: '0vw'})
-        //             $(".link-info-wrap").fadeOut();
-        //             $(".col-2, .col-3, .col-4").fadeIn();
-        //     });
-        //
-        // });
-        //
-        // 
+        $(document).ready(function(){
+            $("a.inline").click(function(){
+               $("body").css({ overflow: "hidden" });
+            });        
+        });
