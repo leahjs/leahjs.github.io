9a6104f415162947acafd5a541f879073a0903b3
leahjs <leahj.suter@gmail.com>
2017-01-12 14:22:28 -0500

    updated js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
         $(document).ready(function(){
             $("a img").click(function(){
                $("body").css({ "overflow": "hidden" });
-            });
-            $('body').click(function(){
-               $("body").css({ "overflow": "auto" });
+               
+               $(this).click(function(){
+                 $("body").css({ "overflow": "auto" });
+               });
             });
         });
