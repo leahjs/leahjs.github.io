64d149f57965912f13711480929c81c0d392e7cb
leahjs <leahj.suter@gmail.com>
2017-01-15 23:33:30 -0500

    updated all pages

--- javascript.js
+++ javascript.js
@@ -15,3 +15,12 @@ $.featherlight({
         document.body.style.overflow = '';
     }
 });
+
+ $('a.gallery').featherlightGallery({
+        previousIcon: '&#9664;',     /* Code that is used as previous icon */
+        nextIcon: '&#9654;',         /* Code that is used as next icon */
+        galleryFadeIn: 100,          /* fadeIn speed when slide is loaded */
+        galleryFadeOut: 300          /* fadeOut speed before slide is loaded */
+    });
+
+      // autoBind: '[data-featherlight-gallery]'
